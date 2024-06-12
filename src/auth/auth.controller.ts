c9296bc4398b52571bb3f6d7bf2d29136eba6531
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-12 08:23:44 +0100

    controllers

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,37 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Patch, Post } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
+import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginDto } from './dto/login.dto';
+import { ChangePasswordDto } from './dto/change-password.dto';
+import { RegisterAdminDto } from './dto/register-admin.dto';
 
+@ApiTags('auth-endpoints')
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @Post('register')
+  @ApiOperation({ summary: 'Register a recruiter or developer' })
+  registerUser(@Body() dto: RegisterUserDto) {
+    return this.authService.registerUser(dto);
+  }
+
+  @Post('login')
+  @ApiOperation({ summary: 'login a user' })
+  loginUser(@Body() dto: LoginDto) {
+    return this.authService.login(dto);
+  }
+
+  @Patch('change-password')
+  @ApiOperation({ summary: 'changer user password' })
+  changePassword(@Body() dto: ChangePasswordDto) {
+    return this.authService.changePassword(dto);
+  }
+
+  @Post('/admin/register')
+  @ApiOperation({ summary: 'Register an admin' })
+  registerAdmin(@Body() dto: RegisterAdminDto) {
+    return this.authService.registerAdmin(dto);
+  }
+}
