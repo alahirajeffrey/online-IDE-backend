e6e2b644bc696e0874a4b71a071cb046579b2e76
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-12 08:54:07 +0100

    commit

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  // setup validation
+  // setup input validation
   app.useGlobalPipes(
     new ValidationPipe({
       whitelist: true,
