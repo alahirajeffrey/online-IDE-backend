58d735667a20fddb9d63ef6c3fee375e1547121e
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-12 08:33:02 +0100

    updated change password dto

--- src/auth/dto/change-password.dto.ts
+++ src/auth/dto/change-password.dto.ts
@@ -1,5 +1,11 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsNotEmpty, IsString } from 'class-validator';
+import {
+  IsNotEmpty,
+  IsString,
+  Matches,
+  MaxLength,
+  MinLength,
+} from 'class-validator';
 
 export class ChangePasswordDto {
   @IsString()
@@ -10,5 +16,17 @@ export class ChangePasswordDto {
   @IsString()
   @IsNotEmpty()
   @ApiProperty()
+  @MinLength(8, {
+    message:
+      'Password is too short. Minimal length is $constraint1 characters, but actual is $value',
+  })
+  @MaxLength(20, {
+    message:
+      'password is too long. Maximal length is $constraint1 characters, but actual is $value',
+  })
+  @Matches(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?!.*\s).{8,20}$/, {
+    message:
+      'password must contain the following: a capital letter, a small letter, and a number',
+  })
   newPassword: string;
 }
