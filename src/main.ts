732a07c8d336d8fea5317c564c04bfda62478064
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-12 10:28:26 +0100

    setup app to enable cors, and protect from csrf attacks

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ValidationPipe } from '@nestjs/common';
+import * as csurf from 'csurf';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // setup protection from Cross-site request forgery attack
+  app.use(csurf());
 
   // setup input validation
   app.useGlobalPipes(
@@ -13,6 +17,9 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  // setup cors
+  app.enableCors();
 
   // add api global prefix
   app.setGlobalPrefix('api/v1');
