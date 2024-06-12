385e9e67d2a86bda9f839c4f1e632c1614f95c94
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-12 10:28:52 +0100

    imported requirments

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,34 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { CloudinaryModule } from './cloudinary/cloudinary.module';
+import { UploadController } from './upload/upload.controller';
+import { UploadModule } from './upload/upload.module';
+import { UploadService } from './upload/upload.service';
+import { PrismaService } from './prisma.client';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true }), AuthModule],
-  controllers: [],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    AuthModule,
+    CloudinaryModule,
+    UploadModule,
+    // setup rate limiting. only 20 requests allowed per minute per client
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 20,
+      },
+    ]),
+  ],
+  controllers: [UploadController],
+  providers: [
+    UploadService,
+    PrismaService,
+    // bind rate limiter guard to entire API
+    { provide: APP_GUARD, useClass: ThrottlerGuard },
+  ],
 })
 export class AppModule {}
