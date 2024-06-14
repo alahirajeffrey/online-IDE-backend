624ab77158a830d92f4b70ae5d9f52bd8bb09344
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-14 03:34:39 +0100

    added controller to get user profile and submission stat

---
+++ src/profile/profile.controller.ts
@@ -0,0 +1,26 @@
+import { Controller, Get, Query, Req, UseGuards } from '@nestjs/common';
+import { ProfileService } from './profile.service';
+import { JwtGuard } from 'src/auth/guards/jwt.guard';
+import { ApiOperation, ApiSecurity, ApiTags } from '@nestjs/swagger';
+
+@ApiTags('profile-endpoints')
+@Controller('profile')
+export class ProfileController {
+  constructor(private profileService: ProfileService) {}
+
+  @UseGuards(JwtGuard)
+  @ApiSecurity('JWT-auth')
+  @Get('own')
+  @ApiOperation({ summary: 'get own profile along with statictics' })
+  getOwnProfileAndStatistics(@Req() req) {
+    return this.profileService.getOwnProfileAndStatistics(req.user.userId);
+  }
+
+  @UseGuards(JwtGuard)
+  @ApiSecurity('JWT-auth')
+  @Get('other')
+  @ApiOperation({ summary: 'get other user profile along with statictics' })
+  getUserProfileAndStatistics(@Query('email') email: string) {
+    return this.profileService.getUserProfileAndStatistics(email);
+  }
+}
