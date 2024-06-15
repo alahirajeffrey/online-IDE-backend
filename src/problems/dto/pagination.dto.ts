03757e2723f4b7aa2fd434985f0cf4fd0562bbcc
alahirajeffrey <alahirajeffrey@gmail.com>
2024-06-15 14:11:40 +0100

    transformed fields in pagination dto

--- src/problems/dto/pagination.dto.ts
+++ src/problems/dto/pagination.dto.ts
@@ -1,12 +1,17 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsOptional } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsNumber, IsOptional } from 'class-validator';
 
 export class PaginationDto {
+  @IsNumber()
   @IsOptional()
   @ApiProperty({ default: 1 })
+  @Type(() => Number)
   page: number;
 
+  @IsNumber()
   @IsOptional()
   @ApiProperty({ default: 10 })
+  @Type(() => Number)
   pageSize: number;
 }
